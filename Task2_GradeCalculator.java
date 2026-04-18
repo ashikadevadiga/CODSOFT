f34011f0a4425b6f2df58d67ad033ad2d7df24e8
ashikadevadiga <ashikadevadiga30@gmail.com>
2026-04-18 22:52:43 +0530

    Add files via upload

---
+++ Task2_GradeCalculator.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Task2_GradeCalculator {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter marks for 5 subjects: ");
+
+        int total = 0;
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("Subject " + i + ": ");
+            int marks = sc.nextInt();
+            total += marks;
+        }
+
+        double average = total / 5.0;
+
+        System.out.println("Total Marks: " + total);
+        System.out.println("Average: " + average);
+
+        if (average >= 90) {
+            System.out.println("Grade: A");
+        } else if (average >= 80) {
+            System.out.println("Grade: B");
+        } else if (average >= 70) {
+            System.out.println("Grade: C");
+        } else if (average >= 60) {
+            System.out.println("Grade: D");
+        } else {
+            System.out.println("Grade: F");
+        }
+    }
+}
